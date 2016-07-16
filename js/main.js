a1ebae79427509a0974d27cfeadbdf158414ecb2
Martijn-Schoenmaker <martijnn@live.nl>
2016-07-16 22:07:58 +0200

    Focus

--- js/main.js
+++ js/main.js
@@ -1,39 +1,36 @@
-var userInput, btnGenerate, generatedCode, assetTag, btnAttribute, copyMessage;
+window.onload = function() {
+  userInput.focus();
+};
+
+var userInput, btnGenerate, generatedCode, assetTag, btnAttribute, copyMessage, cleanCode;
 
 userInput = document.getElementById('asset-tag-name');
 btnGenerate = document.getElementById('btn-generate');
 generatedCode = document.getElementById('generated-code');
 copyMessage = document.getElementById('copy-message');
+history = document.getElementById('history');
 
 function getInput(){
    assetTag = userInput.value;
    filler( assetTag );
 }
 
-function enterPress(e){
-  if(e.keyCode === 13){
-    btnGenerate.click();
-    window.setTimeout(function(){
-          userInput.focus();
-  }, 20);
-
-  }
-
-  return false;
-}
-
-window.onload = function() {
-  userInput.focus();
-};
-
 function filler( layerName ) {
   var strongLayer = '<span>' + layerName + '</span>';
   var strongCode = strongLayer + '.png, 200% ' + strongLayer + '@2x.png, 300% ' + strongLayer + '@3x.png';
-  var cleanCode = layerName + '.png, 200% ' + layerName + '@2x.png, 300% ' + layerName + '@3x.png';
+  cleanCode = layerName + '.png, 200% ' + layerName + '@2x.png, 300% ' + layerName + '@3x.png';
 
   generatedCode.innerHTML = strongCode;
   document.getElementById('btn-generate').dataset.clipboardText = cleanCode;
 
+}
+
+function enterPress(e){
+  if(e.keyCode === 13){
+    btnGenerate.click();
+
+  }
+  return false;
 }
 
 function success() {
@@ -48,20 +45,33 @@ var cpb = clipboardButton('#btn-generate', success, fail);
 
 function message(){
   if (userInput.value === '') {
-    btnGenerate.innerHTML = "Cannot copy nothing!";
-    btnGenerate.style.opacity = "0.5";
+    copyMessage.innerHTML = "Nothing to copy!";
+    copyMessage.style.opacity = "1";
       window.setTimeout(function(){
-        btnGenerate.style.opacity = "1";
-        btnGenerate.innerHTML = "Copy to Clipboard";
-    }, 1500);
+        copyMessage.style.opacity = "0";
+    }, 1000);
   } else {
-    btnGenerate.innerHTML = "Copied!";
-    btnGenerate.style.opacity = "0.5";
+    copyMessage.innerHTML = "Copied!";
+    copyMessage.style.opacity = "0.5";
       window.setTimeout(function(){
-        btnGenerate.style.opacity = "1";
-        btnGenerate.innerHTML = "Copy to Clipboard";
+        copyMessage.style.opacity = "0";
 
-    }, 1500);
+    }, 1000);
   }
-
 }
+
+function generate() {
+  document.getElementById('history').innerHTML += cleanCode;
+  document.getElementById('history').innerHTML += '<br>';
+  document.getElementById('asset-tag-name').focus();
+  message();
+}
+
+function checkFocus() {
+  if (document.hasFocus() === false && userInput.value !== '') {
+    userInput.value = '';
+    generatedCode.innerHTML = 'Generated Code';
+  }
+}
+
+setInterval(checkFocus, 200);
